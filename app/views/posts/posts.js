29da27f242061777e2e3093c13529eed1adbcc9b
robertBrem <brem.rob@gmail.com>
2015-04-28 06:19:52 +0200

    findByUrlTitle

--- app/views/posts/posts.js
+++ app/views/posts/posts.js
@@ -3,19 +3,19 @@
 var entriesApp = angular.module('myApp.posts', ['ngRoute', 'ngSanitize', 'configuration']);
 
 entriesApp.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/posts/:id', {
+    $routeProvider.when('/posts/:urlTitle', {
         templateUrl: 'views/posts/posts.html',
         controller: 'PostsCtrl'
     });
 }]);
 
 entriesApp.controller('PostsCtrl', function ($scope, $http, $routeParams, ENTRY_URL) {
-    $http.get(ENTRY_URL + $routeParams.id).then(function (response) {
+    $http.get(ENTRY_URL + $routeParams.urlTitle).then(function (response) {
         $scope.entry = response.data;
     });
 
     $scope.create = function (comment) {
-        $http.post(ENTRY_URL + $routeParams.id + '/comments',
+        $http.post(ENTRY_URL + $routeParams.urlTitle + '/comments',
             comment)
             .then(function (response) {
                 $scope.entry.comments.push(response.data);
